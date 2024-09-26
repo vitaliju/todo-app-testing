6b5c2f152a4e83e97515d2093e4771ab04fb0123
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-09-26 14:57:28 +0300

    update

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -44,10 +44,71 @@ describe('Adding new tasks', () => {
     cy.shouldContainTask('Go to the gym');
   });
 
-  it('marks tasks as completed', () => {
+  it('displays only active tasks', () => {
     cy.addTask('Go to the gym');
     cy.get('.todo-list li .toggle').click();
     cy.get('.todo-list li').should('have.class', 'completed');
+    cy.addTask('Wash the car');
+    cy.get('a[href="#/active"]').click();
+    cy.get('.todo-list li').should('contain', 'Wash the car');
+  });
+
+  it('displays all tasks if "All" is clicked', () => {
+    cy.addTask('Go to the gym');
+    cy.get('.todo-list li .toggle').click();
+    cy.addTask('Wash the car');
+    cy.addTask('Go shopping');
+    cy.get('a[href="#/"]').click();
+    cy.get('.todo-list li').should('have.length', 3);
+    cy.get('.todo-list li').should('contain', 'Go to the gym');
+    cy.get('.todo-list li').should('contain', 'Wash the car');
+    cy.get('.todo-list li').should('contain', 'Go shopping');
+  });
+
+  it('marks task as completed', () => {
+    cy.addTask('Go to the gym');
+    cy.get('.todo-list li .toggle').click();
+    cy.get('.todo-list li').should('have.class', 'completed');
+  });
+
+  it('marks all tasks as completed', () => {
+    cy.addTask('Go to the gym');
+    cy.addTask('Wash the car');
+    cy.addTask('Go shopping');
+    cy.get('#toggle-all').check({ force: true });
+    cy.get('.todo-list li .toggle').should('be.checked');
+    cy.get('.todo-list li').should('have.class', 'completed');
+  });
+
+  it('deletes a task from the list if button X is clicked', () => {
+    cy.addTask('Go shopping');
+    cy.get('button.destroy').click({ force: true });
+    cy.get('.ng-scope').should('not.contain', 'Go shopping');
+  });
+
+  it('does not deletes uncompleted tasks if "Clear" button is clicked', () => {
+    cy.addTask('Go to the gym');
+    cy.addTask('Wash the car');
+    cy.contains('Clear').click();
+    cy.get('.todo-list li').should('have.length', 2);
+    cy.get('.todo-list li').first().should('contain', 'Go to the gym');
+    cy.get('.todo-list li').last().should('contain', 'Wash the car');
+  });
+
+  it('deletes completed tasks if "Clear" button is clicked', () => {
+    cy.addTask('Go shopping');
+    cy.get('.todo-list  li .toggle').click();
+    cy.contains('Clear').click();
+    cy.addTask('Wash the car');
+    cy.get('.todo-list li').should('contain', 'Wash the car').and('not.contain', 'Go shopping');
+  });
+
+  it('can edit task that is in the list', () => {
+    cy.addTask('Go to the gym');
+    cy.shouldContainTask('Go to the gym');
+    cy.get('.todo-list li label').dblclick();
+    cy.get('input.edit').type('Go shopping{enter}');
+    cy.shouldContainTask('Go shopping');
   });
 });
 
